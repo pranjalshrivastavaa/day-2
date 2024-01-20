26d8991283ae038f221a06877ea5484cc6743305
pranjalshrivastavaa <157197724+pranjalshrivastavaa@users.noreply.github.com>
2024-01-20 07:30:45 +0000

    Pending changes exported from your codespace

--- app.py
+++ app.py
@@ -5,3 +5,16 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("index.html", title="Hello")
+
+@app.route("/hello")
+def hello():
+    return "hello"
+
+@app.route("/name")
+def name():
+    return "pranjal"
+
+@app.route("/surname")
+def surname():
+    return "shrivastava"
+
